1ef79920eab65ca34bad862d240ebc3bdcb6d960
plus7c <1959512279@qq.com>
2022-10-07 17:04:02 +0800

    second update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title:"first site"
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", 
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: `blog`,
+      path: `${__dirname}/blog`,
+    }
+  },
+  "gatsby-plugin-mdx",
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
+import Seo from '../components/seo'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pic2.zhimg.com/v2-a2ac9af669bae907c93a016139e3356e_r.jpg?source=1940ef5c"
+      />
     </Layout>
-  )
-}
+  );
+};
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
-export default IndexPage
+export default IndexPage;
