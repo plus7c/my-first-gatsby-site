2b04d9fdc83ad206b5a1e6e42292e693200ba28f
plus7c <1959512279@qq.com>
2022-10-09 21:27:51 +0800

    version_1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,18 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-    title:"first site"
+    title: "My Super Cool Blog",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", 
-  {
-    resolve: "gatsby-source-filesystem",
-    options: {
-      name: `blog`,
-      path: `${__dirname}/blog`,
-    }
-  },
-  "gatsby-plugin-mdx",
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp"
   ],
 };
